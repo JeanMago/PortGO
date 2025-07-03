8b1a8a7ccd69d0ffce1fcd6dd37328781ea7bb25
JeanMago <128553392+JeanMago@users.noreply.github.com>
2025-07-03 20:53:42 -0300

    ajuste rota

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route  } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
@@ -27,7 +27,7 @@ const App = () => (
       <AuthProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <Router>
           <Routes>
             <Route path="" element={<Index />} />
             <Route path="/PortGO" element={<Index />} />
@@ -44,7 +44,7 @@ const App = () => (
             <Route path="/rules" element={<RulesPage />} /> {/* Adicionar a rota para a página de regras */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
